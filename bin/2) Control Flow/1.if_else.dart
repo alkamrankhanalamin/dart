3a1a8883d812b110f08ee60f4a7c6c671f86e4b0
Al Kamran Khan <alkamrankhanalamin@gmail.com>
2025-11-01 10:42:02 +0600

    loop Extra added

--- bin/2) Control Flow/1.if_else.dart
+++ bin/2) Control Flow/1.if_else.dart
@@ -11,4 +11,16 @@ main() {
   // Ternary Operator
   // condition ? value_if_true : value_if_false;
 
+  //Nested Ternary
+  int mark = 49;
+  mark == 0 ? print("Your marks is very low, You got 0")
+      : mark<33
+      ? print('Your fail')
+      : mark >32 && mark<40
+      ? print('D')
+      : mark>39 && mark<50
+      ? print('C')
+      : print('Invalid Mark');
+
+
 }
